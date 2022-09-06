d52525faeaa523b44fe7a486f2cbfce9792c8c64
Murilo Martins <murilomartins@outlook.com>
2022-09-06 16:36:04 -0300

    :construction: Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,76 +1,88 @@
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../NotificationButton";
+import "./styles.css";
 
 function SalesCard() {
-    return(
-        <div className="dsmeta-sales-card">
-                    <h2>Vendas</h2>
-                    <div className="date-selector">
-                        <div className="dsmeta-form-control-container">
-                            <input className="dsmeta-form-control" type="date" />
-                        </div>
-                        <div className="dsmeta-form-control-container">
-                            <input className="dsmeta-form-control" type="date" />
-                        </div>
-                    </div>
-                    <div className="dsmeta-sales-table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th className="show-992">ID</th>
-                                    <th className="show-576">Data</th>
-                                    <th>Vendedor</th>
-                                    <th className="show-992">Visitas</th>
-                                    <th className="show-992">Vendas</th>
-                                    <th>Total</th>
-                                    <th>Notificar</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className="table-row">
-                                    <td className="show-992">1</td>
-                                    <td className="show-576">20/03/2022</td>
-                                    <td>José Alface</td>
-                                    <td className="show-992">12</td>
-                                    <td className="show-992">23</td>
-                                    <td>200.30</td>
-                                    <td>
-                                        <div className="notification-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr className="table-row">
-                                    <td className="show-992">1</td>
-                                    <td className="show-576">20/03/2022</td>
-                                    <td>José Alface</td>
-                                    <td className="show-992">12</td>
-                                    <td className="show-992">23</td>
-                                    <td>200.30</td>
-                                    <td>
-                                        <div className="notification-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr className="table-row">
-                                    <td className="show-992">1</td>
-                                    <td className="show-576">20/03/2022</td>
-                                    <td>José Alface</td>
-                                    <td className="show-992">12</td>
-                                    <td className="show-992">23</td>
-                                    <td>200.30</td>
-                                    <td>
-                                        <div className="notification-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>                                
-                        </table>
-                    </div>
+  return (
+    <div className="dsmeta-sales-card">
+      <h2>Vendas</h2>
+      <div className="date-selector">
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+        <div className="dsmeta-form-control-container">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
+        </div>
+      </div>
+      <div className="dsmeta-sales-table">
+        <table>
+          <thead>
+            <tr>
+              <th className="show-992">ID</th>
+              <th className="show-576">Data</th>
+              <th>Vendedor</th>
+              <th className="show-992">Visitas</th>
+              <th className="show-992">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-row">
+              <td className="show-992">1</td>
+              <td className="show-576">20/03/2022</td>
+              <td>José Alface</td>
+              <td className="show-992">12</td>
+              <td className="show-992">23</td>
+              <td>200.30</td>
+              <td>
+                <div className="notification-container">
+                  <NotificationButton />
                 </div>
-    ) 
-  }
-  
-  export default SalesCard
+              </td>
+            </tr>
+            <tr className="table-row">
+              <td className="show-992">1</td>
+              <td className="show-576">20/03/2022</td>
+              <td>José Alface</td>
+              <td className="show-992">12</td>
+              <td className="show-992">23</td>
+              <td>200.30</td>
+              <td>
+                <div className="notification-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr className="table-row">
+              <td className="show-992">1</td>
+              <td className="show-576">20/03/2022</td>
+              <td>José Alface</td>
+              <td className="show-992">12</td>
+              <td className="show-992">23</td>
+              <td>200.30</td>
+              <td>
+                <div className="notification-container">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default SalesCard;
