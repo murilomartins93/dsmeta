2a52e495659d1311be41401e43c8b2d75e613ada
Murilo Martins <murilomartins@outlook.com>
2022-09-09 18:18:52 -0300

    :construction: Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -72,7 +72,7 @@ function SalesCard() {
                   <td>{sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="notification-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
